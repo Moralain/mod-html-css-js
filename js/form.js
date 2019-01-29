f0044d24d707f4030c688fa47cb7acdca6139c4b
Moralain <jormoluis@gmail.com>
2019-01-29 20:37:58 +0000

    upload files

--- js/form.js
+++ js/form.js
@@ -1,14 +1,40 @@
 export function setForm() {
 
-    let button = document.getElementById("otros");
-    let paragraph = document.getElementById("otherOpt");
+    let form = document.querySelector('#contactFormID');
+    let oContact = {}
+    form.addEventListener('submit', enviar);
+
+    function enviar(oEv){
+        oEv.preventDefault();
+
+        oContact.name = document.querySelector('#name').value;
+        oContact.email = document.querySelector('#email').value;
+        oContact.phone = document.querySelector('#phone').value;
+        oContact.others = document.querySelector('#others').value;
+        oContact.feedback = document.querySelector('#feedback').value;
+        oContact.radios = getRadio(document.querySelectorAll('[name="radios"]'));
+
+        console.log(oContact)
+
+    }
+
+    function getRadio(aNodos){
+        console.dir(aNodos);
+        for(let i = 0; i < aNodos.length; i++){
+            const item = aNodos[i]
+            if(item.checked){return item.value}
+        }
+    }
+    
+    // let button = document.getElementById("otros");
+    // let paragraph = document.getElementById("otherOpt");
     
 
-    button.addEventListener("click", function(){
-        if (paragraph.style.display == 'none') {
-            paragraph.style.display = 'block';
-        } else {
-            paragraph.style.display = 'none';
-        }
-    }, false);
+    // button.addEventListener("click", function(){
+    //     if (paragraph.style.display == 'none') {
+    //         paragraph.style.display = 'block';
+    //     } else {
+    //         paragraph.style.display = 'none';
+    //     }
+    // }, false);
 }    
--- js/menu.js
+++ js/menu.js
@@ -1,78 +1,38 @@
 export function setMenu() {
-// Smooth menu
-(function() {
+/*--- PAINT MENU OPTIONS ---*/ 
+    let aMenuItems = document.querySelectorAll("nav.main-menu a");
+    let aSections = document.querySelectorAll("section");
+    let oOffsets = [];
+    prepararNavegacion()
+    window.addEventListener('scroll', changeMenuStyle);
+    function prepararNavegacion(){
+        aSections.forEach(
+            (item) => oOffsets['#'+item.id] = item.offsetTop
+        )
+    }
+    function changeMenuStyle(){
+        let pageOffset = window.pageYOffset;
+        let menuItem = 0; 
+        if( pageOffset >= oOffsets['#home'] && pageOffset < oOffsets['#whoIam'] ){
+            menuItem = 0;
+        } else if( pageOffset >= oOffsets['#whoIam'] && pageOffset < oOffsets['#studies'] ){
+            menuItem = 1;
+        } else if( pageOffset >= oOffsets['#studies'] && pageOffset < oOffsets['#experience'] ){
+            menuItem = 2;
+        } else if( pageOffset >= oOffsets['#experience'] && pageOffset < oOffsets['#aboutMe'] ){
+            menuItem = 3;
+        } else if( pageOffset >= oOffsets['#aboutMe'] && pageOffset < oOffsets['#contact'] ){
+            menuItem = 4;
+        }
+        else{
+            menuItem = 5;
+        }
+        aMenuItems.forEach(
+            (item) => item.classList.remove('active')
+        )
+        aMenuItems[menuItem].classList.add('active')
+    }
 
-    'use strict';
-
-   // Feature Test
-   if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-
-       // Función que anima el scroll
-       let smoothScroll = function (anchor, duration) {
-
-           //  Calcular la distancia y la velocidad de desplazamiento del scroll
-           let startLocation = window.pageYOffset;
-           let endLocation = anchor.offsetTop;
-           let distance = endLocation - startLocation;
-           let increments = distance/(duration/16);
-           let stopAnimation;
-
-           // Desplazarse por la página en incremento y verificar si es el momento de detenerse
-           let animateScroll = function () {
-               window.scrollBy(0, increments);
-               stopAnimation();
-           };
-
-           // Si se desplaza hacia abajo
-           if ( increments >= 0 ) {
-               // Detener la animación cuando llegue al punto de ancla o la parte inferior de la página
-               stopAnimation = function () {
-                   let travelled = window.pageYOffset;
-                   if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-                       clearInterval(runAnimation);
-                   }
-               };
-           }
-           // Si se desplaza hacia arriba
-           else {
-               // Detener la animación cuando llegue el punto de ancla o la parte superior de la página
-               stopAnimation = function () {
-                   let travelled = window.pageYOffset;
-                   if ( travelled <= (endLocation || 0) ) {
-                       clearInterval(runAnimation);
-                   }
-               };
-           }
-
-           // Bucle de la función de animación
-           let runAnimation = setInterval(animateScroll, 16);
-      
-       };
-
-       // Definir los enlaces del scroll
-       let scrollToggle = document.querySelectorAll('a.scroll');
-
-       // Para cada enlace del scroll
-       [].forEach.call(scrollToggle, function (toggle) {
-
-           // Cuando se hace clic en el enlace del scroll
-           toggle.addEventListener('click', function(e) {
-
-               // Prevenir el comportamiento predeterminado
-               e.preventDefault();
-
-               // Obtener enlace del punto de ancla y calcular la distancia desde la parte superior
-               let dataID = toggle.getAttribute('href');
-               let dataTarget = document.querySelector(dataID);
-               let dataSpeed = toggle.getAttribute('data-speed');
-
-               // Si el punto de ancla existe
-               if (dataTarget) {
-                   // Desplazarse hasta el punto de ancla
-                   smoothScroll(dataTarget, dataSpeed || 500);
-               }
-           }, false);
-       });
-   }
-})();
+    /*--- SMOOTH SCROLL ---*/ 
+    
 }
